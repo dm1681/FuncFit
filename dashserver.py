32f391c2179472addd57e2cdeea17bae00eb0f35
Diego <dm1681@gmail.com>
2021-05-13 00:38:59 +0100

    first commit, standing dashserver hosting an editable DataTable

---
+++ dashserver.py
@@ -0,0 +1,135 @@
+#! /usr/local/opt/python-3.9.4/bin/python3.9
+"""
+This application serves as a way of maniuplating a pandas dataframe for each user to update the work outs saved.
+The graphs will then update dynamically.
+
+
+To do:
+    - serve figures with various options (raw numbers, weighted by body weight)
+    - accept Apple Health XML to update weight
+"""
+# first party
+import os
+from pathlib import Path
+import pdb
+import logging
+from typing import Optional
+
+# third party
+import dash
+from dash import Dash 
+from dash.dependencies import Input, Output, State
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_table
+import pandas as pd
+
+app = Dash("Funcitonal Fitness", )
+logger = logging.getLogger(__file__)
+
+app.layout = html.Div([
+    html.Button("Add Row", id='add-row-button', n_clicks=0),
+    html.Button("Add Column", id='add-column-button', n_clicks=0),
+    html.Button("Save",id='save-button', n_clicks=0),
+    dash_table.DataTable(
+        id='exercise-table',
+        sort_action="native",
+        row_selectable='multi',
+        row_deletable=True,
+        filter_action='native',
+        ),
+    dcc.Store(id='exercise-data'),
+    html.Div(id='hidden-div', hidden=True)
+
+])
+
+# read in file, if it doesnt exist, make it
+def make_dataframe(file_name:Path):
+    """
+    Fetches the local dataframe for the user if it exists, else create a new one.
+
+    Args:
+        file_name (Path): Path dataframe *should* exist at.
+
+    Returns:
+        pd.DataFrame: Loaded DataFrame for that user. 
+    """
+
+    logger.info("Making DataFrame")
+    if file_name.exists():
+        logger.info(f"Reading DataFrame from file {file_name}")
+        df = pd.read_csv(file_name, index_col=0)
+
+    else:
+        logger.info(f"Creating DataFrame and saving to file {file_name}")
+        df = pd.DataFrame()
+        df['Date'] = []
+        df['Exercise'] = []
+        df['Weight'] = []
+        df['Sets'] = []
+        df['Reps'] = []
+        df['Volume'] = []
+        df['Muscle Group'] = []
+
+        df.to_csv(file_name)
+    return df
+
+
+# update dataframe depending on button press
+@app.callback(Output('exercise-data','data'),
+              [Input('add-column-button', 'n_clicks'),
+               Input('add-row-button', 'n_clicks')],
+              [State('exercise-data','data')])
+def update_dataframe(col_but:int, row_but:int, data:Optional[str]):
+
+    ctx = dash.callback_context
+
+    # load in DataFrame if it doesnt already exist.
+    if data is None:
+        df = make_dataframe(Path("diego.csv"))
+
+    # data already exists, load it in.
+    else:
+        df = pd.read_json(data) 
+
+
+
+    # parse context of this callback
+    if ctx.triggered[0]['prop_id'] == 'add-column-button.n_clicks':
+        df['New Column'] = [None] * len(df)
+    elif ctx.triggered[0]['prop_id'] == 'add-row-button.n_clicks':
+        df = df.append(pd.Series(dtype='object'), ignore_index=True)
+    else:
+        print("idk...")
+    
+    # send to dcc.Store as json.
+    return df.to_json() 
+
+
+
+@app.callback([Output('exercise-table', 'columns'),
+               Output('exercise-table', 'data')],
+             [Input('exercise-data', 'data')], prevent_initial_call=True)
+def update_datatable(raw_df:Optional[str]):
+
+    df = pd.read_json(raw_df)
+
+    columns=[{'name': col , 'id':col, 'renamable':True, 'editable':True} for col in df.columns] 
+    data = df.values if df.empty else df.to_dict('records')
+
+
+    return columns, data
+
+
+@app.callback(Output('hidden-div', 'children'),
+             [Input('save-button', 'n_clicks')],
+             [State('exercise-table','data')], prevent_initial_call=True)
+def save_datatable(save_nClicks, data_table):
+    df = pd.DataFrame(data_table)
+    df.to_csv(Path('user.csv'))
+    return True
+
+
+
+
+app.run_server(host="0.0.0.0", port="8050", debug=True)
